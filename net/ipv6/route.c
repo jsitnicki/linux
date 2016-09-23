3f400fb65dc378f845c9aac75da385a953e3f1d0
Jakub Sitnicki <jkbs@redhat.com>
2016-09-23 18:36:35 +0200

    ipv6: Use multipath hash from flow info if available
    Allow our callers to influence the choice of ECMP link by honoring the
    hash passed together with the flow info. This allows for special
    treatment of ICMP errors which we would like to route over the same path
    as the IPv6 datagram that triggered the error.
    
    Also go through rt6_multipath_hash(), in the usual case when we aren't
    dealing with an ICMP error, so that there is one central place where
    multipath hash is computed.
    
    Signed-off-by: Jakub Sitnicki <jkbs@redhat.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -455,7 +455,10 @@ static struct rt6_info *rt6_multipath_select(struct rt6_info *match,
 	struct rt6_info *sibling, *next_sibling;
 	int route_choosen;
 
-	route_choosen = get_hash_from_flowi6(fl6) % (match->rt6i_nsiblings + 1);
+	if (!fl6->mp_hash)
+		fl6->mp_hash = rt6_multipath_hash(fl6, NULL);
+
+	route_choosen = fl6->mp_hash % (match->rt6i_nsiblings + 1);
 	/* Don't change the route, if route_choosen == 0
 	 * (siblings does not include ourself)
 	 */
