cec474bce5169fa19a51b2f5029fef1ae88b2843
Jakub Sitnicki <jakub@cloudflare.com>
2019-10-17 16:08:21 +0200

    WIP: bpf: Tests for BPF socket dispatch with UDP
    Signed-off-by: Jakub Sitnicki <jakub@cloudflare.com>

--- tools/testing/selftests/bpf/test_inet_lookup.c
+++ tools/testing/selftests/bpf/test_inet_lookup.c
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * L7 echo tests with the server listening on a different (address,
- * port) than the client sends packets to.
+ * L7 echo tests with the server listening/receiving at a different
+ * (address, port) than the client sends packets to.
  *
  * Traffic is steered to the server socket by programming the socket
  * lookup with a BPF inet_lookup program. The inet_lookup program
- * selects the listening socket from a user-space supplied SOCKMAP
- * based on the packet's 4-tuple.
+ * selects the listening/receiving socket from a user-space supplied
+ * SOCKMAP based on the packet's 4-tuple.
  */
 
 #include <arpa/inet.h>
@@ -90,6 +90,41 @@ static const struct test tests[] = {
 		.recv_at	= { INT_IP4_V6, INT_PORT },
 		.send_to	= { EXT_IP4, EXT_PORT },
 	},
+	{
+		.desc		= "UDP IPv4 redir port",
+		.bpf_prog	= "inet_lookup/redir_port",
+		.socket_type	= SOCK_DGRAM,
+		.send_to	= { EXT_IP4, EXT_PORT },
+		.recv_at	= { EXT_IP4, INT_PORT },
+	},
+	{
+		.desc		= "UDP IPv4 redir addr",
+		.bpf_prog	= "inet_lookup/redir_ip4",
+		.socket_type	= SOCK_DGRAM,
+		.send_to	= { EXT_IP4, EXT_PORT },
+		.recv_at	= { INT_IP4, EXT_PORT },
+	},
+	{
+		.desc		= "UDP IPv6 redir port",
+		.bpf_prog	= "inet_lookup/redir_port",
+		.socket_type	= SOCK_DGRAM,
+		.send_to	= { EXT_IP6, EXT_PORT },
+		.recv_at	= { EXT_IP6, INT_PORT },
+	},
+	{
+		.desc		= "UDP IPv6 redir addr",
+		.bpf_prog	= "inet_lookup/redir_ip6",
+		.socket_type	= SOCK_DGRAM,
+		.send_to	= { EXT_IP6, EXT_PORT },
+		.recv_at	= { INT_IP6, EXT_PORT },
+	},
+	{
+		.desc		= "UDP IPv4->IPv6 redir port",
+		.bpf_prog	= "inet_lookup/redir_port",
+		.socket_type	= SOCK_DGRAM,
+		.recv_at	= { INT_IP4_V6, INT_PORT },
+		.send_to	= { EXT_IP4, EXT_PORT },
+	},
 };
 
 static bool is_ipv6_addr(const char *ip)
@@ -128,6 +163,7 @@ static void make_addr(int family, const char *ip, int port,
 static int make_server(int type, const char *ip, int port)
 {
 	struct sockaddr_storage ss = {0};
+	const int one = 1;
 	int fd, sz;
 	int family;
 
@@ -137,6 +173,14 @@ static int make_server(int type, const char *ip, int port)
 	fd = socket(family, type, 0);
 	if (fd < 0)
 		error(1, errno, "failed to create listen socket");
+
+	if (type == SOCK_DGRAM &&
+	    setsockopt(fd, SOL_IP, IP_RECVORIGDSTADDR, &one, sizeof(one)))
+		error(1, errno, "failed to set IP_RECVORIGDSTADDR");
+
+	if (type == SOCK_DGRAM && family == AF_INET6 &&
+	    setsockopt(fd, SOL_IPV6, IPV6_RECVORIGDSTADDR, &one, sizeof(one)))
+		error(1, errno, "failed to set IPV6_RECVORIGDSTADDR");
 
 	if (bind(fd, (struct sockaddr *)&ss, sz))
 		error(1, errno, "failed to bind listen socket");
@@ -209,10 +253,100 @@ static void tcp_recv_send(int server_fd)
 	close(fd);
 }
 
+static void udp_recv_send(int server_fd)
+{
+	char cmsg_buf[CMSG_SPACE(sizeof(struct sockaddr_storage))];
+	struct sockaddr_storage _src_addr = { 0 };
+	struct sockaddr_storage _dst_addr = { 0 };
+	struct sockaddr_storage *src_addr = &_src_addr;
+	struct sockaddr_storage *dst_addr = NULL;
+	struct msghdr msg = { 0 };
+	struct iovec iov = { 0 };
+	struct cmsghdr *cm;
+	char buf[1];
+	ssize_t n;
+	int fd;
+
+	iov.iov_base = buf;
+	iov.iov_len = sizeof(buf);
+
+	msg.msg_name = src_addr;
+	msg.msg_namelen = sizeof(*src_addr);
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = cmsg_buf;
+	msg.msg_controllen = sizeof(cmsg_buf);
+
+	n = recvmsg(server_fd, &msg, 0);
+	if (n < 0)
+		error(1, errno, "failed to receive");
+	if (n < sizeof(buf))
+		error(1, 0, "partial receive");
+	if (msg.msg_flags & MSG_CTRUNC)
+		error(1, errno, "truncated cmsg");
+
+	for (cm = CMSG_FIRSTHDR(&msg); cm; cm = CMSG_NXTHDR(&msg, cm)) {
+		if ((cm->cmsg_level == SOL_IP &&
+		     cm->cmsg_type == IP_ORIGDSTADDR) ||
+		    (cm->cmsg_level == SOL_IPV6 &&
+		     cm->cmsg_type == IPV6_ORIGDSTADDR)) {
+			dst_addr = (struct sockaddr_storage *)CMSG_DATA(cm);
+			break;
+		}
+		error(0, 0, "ignored cmsg at level %d type %d",
+		      cm->cmsg_level, cm->cmsg_type);
+	}
+	if (!dst_addr)
+		error(1, 0, "failed to get destination address");
+
+	/* Server bound to IPv4-mapped IPv6 address */
+	if (src_addr->ss_family != dst_addr->ss_family) {
+		assert(dst_addr->ss_family == AF_INET);
+
+		struct sockaddr_in *dst4 = (void *)dst_addr;
+		struct sockaddr_in6 *dst6 = (void *)&_dst_addr;
+
+		dst6->sin6_family = AF_INET6;
+		dst6->sin6_port = dst4->sin_port;
+
+		dst6->sin6_addr.s6_addr[10] = 0xff;
+		dst6->sin6_addr.s6_addr[11] = 0xff;
+		memcpy(&dst6->sin6_addr.s6_addr[12],
+		       &dst4->sin_addr.s_addr, sizeof(dst4->sin_addr.s_addr));
+
+		dst_addr = (void *)dst6;
+	}
+
+	/* Reply from original destination address. */
+	fd = socket(dst_addr->ss_family, SOCK_DGRAM, 0);
+	if (fd < 0)
+		error(1, errno, "failed to create socket");
+
+	if (bind(fd, (struct sockaddr *)dst_addr, sizeof(*dst_addr)))
+		error(1, errno, "failed to bind socket");
+
+	msg.msg_control = NULL;
+	msg.msg_controllen = 0;
+	n = sendmsg(fd, &msg, 0);
+	if (n < 0)
+		error(1, errno, "failed to send");
+	if (n < sizeof(buf))
+		error(1, 0, "partial send");
+
+	close(fd);
+}
+
 static void tcp_echo(int client_fd, int server_fd)
 {
 	send_byte(client_fd);
 	tcp_recv_send(server_fd);
+	recv_byte(client_fd);
+}
+
+static void udp_echo(int client_fd, int server_fd)
+{
+	send_byte(client_fd);
+	udp_recv_send(server_fd);
 	recv_byte(client_fd);
 }
 
@@ -335,7 +469,10 @@ static void run_test(const struct test *t, struct bpf_object *obj,
 	client_fd = make_client(t->socket_type,
 				t->send_to.ip, t->send_to.port);
 
-	tcp_echo(client_fd, server_fd);
+	if (t->socket_type == SOCK_STREAM)
+		tcp_echo(client_fd, server_fd);
+	else
+		udp_echo(client_fd, server_fd);
 
 	close(client_fd);
 	close(server_fd);
