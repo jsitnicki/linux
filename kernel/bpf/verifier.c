ba63d29a23bd48336eb4f9cbaf27968f0e047dff
Jakub Sitnicki <jakub@cloudflare.com>
2019-10-21 10:24:56 +0200

    bpf: Allow selecting reuseport socket from a SOCKMAP
    SOCKMAP now supports storing references to listening sockets. Nothing keeps
    us from using it as an array of sockets to select from in BPF reuseport
    programs. Whitelist the map type with the bpf_sk_select_reuseport helper.
    
    The restriction that the socket has to be a member of a reuseport group
    still applies. Socket from a SOCKMAP that does not have sk_reuseport_cb set
    is not a valid target and we signal it with -EINVAL.
    
    This lifts the restriction that SOCKARRAY imposes, if SOCKMAP is used with
    reuseport BPF, the listening sockets can exist in more than one BPF map at
    the same time.
    
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Signed-off-by: Jakub Sitnicki <jakub@cloudflare.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -3693,7 +3693,8 @@ static int check_map_func_compatibility(struct bpf_verifier_env *env,
 		if (func_id != BPF_FUNC_sk_redirect_map &&
 		    func_id != BPF_FUNC_sock_map_update &&
 		    func_id != BPF_FUNC_map_delete_elem &&
-		    func_id != BPF_FUNC_msg_redirect_map)
+		    func_id != BPF_FUNC_msg_redirect_map &&
+		    func_id != BPF_FUNC_sk_select_reuseport)
 			goto error;
 		break;
 	case BPF_MAP_TYPE_SOCKHASH:
@@ -3774,7 +3775,8 @@ static int check_map_func_compatibility(struct bpf_verifier_env *env,
 			goto error;
 		break;
 	case BPF_FUNC_sk_select_reuseport:
-		if (map->map_type != BPF_MAP_TYPE_REUSEPORT_SOCKARRAY)
+		if (map->map_type != BPF_MAP_TYPE_REUSEPORT_SOCKARRAY &&
+		    map->map_type != BPF_MAP_TYPE_SOCKMAP)
 			goto error;
 		break;
 	case BPF_FUNC_map_peek_elem:
