d899c3c85d7b2c4af905bbb4bc84bda8d1d4abce
Arthur Fabre <afabre@cloudflare.com>
2025-01-23 22:22:39 +0100

    ice: Propagate trait presence to skb
    Call the common xdp_buff_update_skb() helper.
    
    Signed-off-by: Arthur Fabre <afabre@cloudflare.com>

--- drivers/net/ethernet/intel/ice/ice_txrx.c
+++ drivers/net/ethernet/intel/ice/ice_txrx.c
@@ -990,6 +990,8 @@ ice_build_skb(struct ice_rx_ring *rx_ring, struct xdp_buff *xdp)
 					   nr_frags * xdp->frame_sz,
 					   xdp_buff_is_frag_pfmemalloc(xdp));
 
+	xdp_buff_update_skb(xdp, skb);
+
 	return skb;
 }
 
@@ -1070,6 +1072,8 @@ ice_construct_skb(struct ice_rx_ring *rx_ring, struct xdp_buff *xdp)
 					   nr_frags * xdp->frame_sz,
 					   xdp_buff_is_frag_pfmemalloc(xdp));
 	}
+
+	xdp_buff_update_skb(xdp, skb);
 
 	return skb;
 }
