8e7beeec3da9f099bb1bc1dd47db21d9a570b4c6
Jakub Sitnicki <jakub@cloudflare.com>
2025-09-14 14:33:54 +0200

    bpf: Make bpf_skb_net_grow metadata-safe
    TODO
    
    Signed-off-by: Jakub Sitnicki <jakub@cloudflare.com>

--- net/core/filter.c
+++ net/core/filter.c
@@ -3251,10 +3251,11 @@ static void bpf_skb_change_protocol(struct sk_buff *skb, u16 proto)
 
 static int bpf_skb_generic_push(struct sk_buff *skb, u32 off, u32 len)
 {
-	/* Caller already did skb_cow() with len as headroom,
+	/* Caller already did skb_cow() with meta_len+len as headroom,
 	 * so no need to do it here.
 	 */
 	skb_push(skb, len);
+	skb_metadata_postpush_move(skb, len);
 	memmove(skb->data, skb->data + len, off);
 	memset(skb->data + off, 0, len);
 
@@ -3488,6 +3489,7 @@ static int bpf_skb_net_grow(struct sk_buff *skb, u32 off, u32 len_diff,
 	u8 inner_mac_len = flags >> BPF_ADJ_ROOM_ENCAP_L2_SHIFT;
 	bool encap = flags & BPF_F_ADJ_ROOM_ENCAP_L3_MASK;
 	u16 mac_len = 0, inner_net = 0, inner_trans = 0;
+	const u8 meta_len = skb_metadata_len(skb);
 	unsigned int gso_type = SKB_GSO_DODGY;
 	int ret;
 
@@ -3498,7 +3500,7 @@ static int bpf_skb_net_grow(struct sk_buff *skb, u32 off, u32 len_diff,
 			return -ENOTSUPP;
 	}
 
-	ret = skb_cow_head(skb, len_diff);
+	ret = skb_cow_head(skb, meta_len + len_diff);
 	if (unlikely(ret < 0))
 		return ret;
 
